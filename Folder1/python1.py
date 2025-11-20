7a39a8f61861d6554e0b1f60c1a4a1168d11f646
lv007-simreka <lokesh_intern@simreka.com>
2025-11-20 12:00:23 +0530

    new code chaged

--- Folder1/python1.py
+++ Folder1/python1.py
@@ -1,4 +1,2 @@
-print("new thing was added")
-print("AGain code was added")
-
 print("new branch is created and pushing it to and later we will add it to the main branch")
+print("Now i have deleted first two lines of code and adding one more line, this is the line..!")
